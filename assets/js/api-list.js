0a6b2d1e95e781f740eff72d6962bf0df11ada93
Yazhong Liu <yorkiefixer@gmail.com>
2015-06-15 12:05:59 +0800

    update sidebar animation

--- assets/js/api-list.js
+++ assets/js/api-list.js
@@ -1,3 +1,13 @@
-(function () {
-  console.log('haha');
-})
+$(document).ready(function () {
+
+  'use strict';
+
+  var itemsList = $('#api-items>li>.api-item-clickable');
+  itemsList.on(
+    'click',
+    function onclick () {
+      $(this).parent().find('.members-list').toggleClass('hidden');
+    }
+  );
+
+});
