9bd37bfb40ac87d4dcf3f41970385548f261ef5b
Yazhong Liu <yorkiefixer@gmail.com>
2015-06-17 22:27:09 +0800

    sort the sidebar list

--- locals.js
+++ locals.js
@@ -4,4 +4,15 @@ module.exports = function (modules, classes, locals) {
   // TODO
   // console.log(modules[0]);
 
+  locals.modules = modules.sort(function (prev, next) {
+    return prev.name > next.name;
+  });
+
+  locals.classes = classes.sort(function (prev, next) {
+    if (prev.module === next.modules)
+      return prev.name > next.name;
+    else
+      return prev.module > next.module;
+  });
+
 };
