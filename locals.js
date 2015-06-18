66343cf669d1a705187e2a4e93cd96a24ab2c790
Yazhong Liu <yorkiefixer@gmail.com>
2015-06-18 12:48:49 +0800

    local: fix sidebar sort

--- locals.js
+++ locals.js
@@ -1,18 +1,14 @@
 
+var _ = require('underscore');
+
 module.exports = function (modules, classes, locals) {
 
-  // TODO
-  // console.log(modules[0]);
-
-  locals.modules = modules.sort(function (prev, next) {
-    return prev.name > next.name;
-  });
-
-  locals.classes = classes.sort(function (prev, next) {
-    if (prev.module === next.modules)
-      return prev.name > next.name;
-    else
-      return prev.module > next.module;
-  });
-
+  locals.modules = _.sortBy(modules, 'name');
+  locals.classes = _.sortBy(
+    _.map(classes, function (clazz) {
+      clazz.ns = (clazz.module || '') + '.' + clazz.name;
+      return clazz;
+    }),
+    'ns'
+  );
 };
