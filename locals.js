f4ed7f18249ba45b2e7fe19d94b5e326ec746695
Nan Wang <nantas@gmail.com>
2015-11-11 20:55:57 +0800

    added i18n localization files. added --source option to include source files. fixed enum display wrong in sidebar.

--- locals.js
+++ locals.js
@@ -1,6 +1,6 @@
 
 var _ = require('underscore');
-
+  
 module.exports = function (modules, classes, locals) {
   locals.modules = _.sortBy(modules, 'namespace');
   locals.classes = _.sortBy(classes, 'namespace');
